599aec640c90da5a02633c2bc228fef8f38c82e8
miasuman <smia34872@gmail.com>
2019-05-19 17:53:07 +0600

    made it

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,13 @@
-console.log("cool");
+function palindrome(str) {
+  const sentence = str.replace(/[\W_]/g, "").toLowerCase();
+  const reversedSentence = sentence
+    .split("")
+    .reverse()
+    .join("");
+  if (reversedSentence === sentence) {
+    return true;
+  }
+  return false;
+}
+
+console.log(palindrome("eye"));
